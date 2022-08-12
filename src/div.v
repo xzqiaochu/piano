142d1b95dba963d3bb9d9463a9ea8c9dc58df7f9
xzqiaochu <xzzhangqiaochu@163.com>
2022-08-12 20:18:38 +0800

    【opt】div模块中改为非阻塞赋值

--- src/div.v
+++ src/div.v
@@ -17,29 +17,29 @@ initial ans = 0;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
-        last_m = 0;
-        last_n = 0;
-        m_cache = 0;
-        n_cache = 0;
-        ans_cache = 0;
-        ans = 0;
+        last_m <= 0;
+        last_n <= 0;
+        m_cache <= 0;
+        n_cache <= 0;
+        ans_cache <= 0;
+        ans <= 0;
     end
     else if (m_cache != 0) begin
         if (m_cache > n_cache) begin
-            m_cache = m_cache - n_cache;
-            ans_cache = ans_cache + 1;
+            m_cache <= m_cache - n_cache;
+            ans_cache <= ans_cache + 1;
         end
         else begin
-            m_cache = 0;
-            ans = ans_cache;
+            m_cache <= 0;
+            ans <= ans_cache;
         end
     end
     else if (m != last_m || n != last_n) begin
-        ans_cache = 0;
-        m_cache = m;
-        n_cache = n;
-        last_m = m;
-        last_n = n;
+        ans_cache <= 0;
+        m_cache <= m;
+        n_cache <= n;
+        last_m <= m;
+        last_n <= n;
     end
 end
 
